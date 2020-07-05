843ba0f238eed3f9b7f0fb0b60526b0f40db216c
Ariel Behar <ariel.behar@hotmail.com>
2020-07-05 18:30:43 +0300

    Update index page Second and Third section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+
+// ANIMATIONS #index-first-section
 document.addEventListener('DOMContentLoaded', () => {
     let mainTitleTextContent = document.querySelector('#index-first-section >div >div > div.text-content');
     let lawyersImg = document.querySelector('.lawyers-images-div > img')
@@ -8,7 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
     mainTitleTextContent.style.opacity = 1;
 
 })
+// Finish ANIMATIONS #index-first-page
 
+// RUDI and STANISH info and population of components
 let accountingServices = ['Данъчна Защита', 'Счетоводство', 'Финансов Одит', 'Представителство (ГИТ, НАП, НОИ, КОНПИ)'];
 
 let lawServices = ['Семейно Право', 'Наказателно Право', 'Гражданско Право', 'Правно Представителство'];
@@ -31,13 +35,11 @@ function indexPageInsertElements(serviceProvider) {
     const serviceProviderDiv = document.querySelector(`#index-page-${serviceProvider.nickname} > .img-holder`)
 
     serviceProviderDiv.insertAdjacentHTML("afterbegin", `<img src="./img/ariel.jpg" alt="${serviceProvider.name} - ${serviceProvider.profession}" class="service-provider-image">`);
-
-    
+  
     const providerName = document.querySelector(`#index-page-${serviceProvider.nickname} > .service-provider-details > .name`);
     const providerProfession = document.querySelector(`#index-page-${serviceProvider.nickname}  > .service-provider-details > .profession`);
     const providerExperience = document.querySelector(`#index-page-${serviceProvider.nickname}  > .service-provider-details > .experience`);
     const providerServices = document.querySelector(`#index-page-${serviceProvider.nickname} > .service-provider-details > .services`);
-
 
     providerName.textContent = serviceProvider.name;
     providerProfession.textContent = serviceProvider.profession;
@@ -45,9 +47,36 @@ function indexPageInsertElements(serviceProvider) {
     serviceProvider.services.forEach(item =>{
         providerServices.innerHTML += `<li>${item}</li>`;
     })
-
 }
-
 
 indexPageInsertElements(rudi);
 indexPageInsertElements(stanish);
+// Finish RUDI and STANISH info and population of components
+
+// #index-third-section
+let choiceQualityMask = document.querySelectorAll('#index-third-section .choice-quality .mask');
+let choiceQualityPar = document.querySelectorAll('#index-third-section .choice-quality > .mask p');
+
+choiceQualityMask.forEach(item =>{
+    item.addEventListener('mouseenter', () => {
+        item.style.backgroundColor = "rgba(0,0,0,0.5)";
+        item.querySelector('h6').style.display = "none";
+
+        item.querySelector('p').style.display = "block";
+        item.querySelector('img').style.display = "block";
+       
+    })
+
+    item.addEventListener('mouseleave', () => {
+        item.style.backgroundColor = "rgba(0,0,0,0.2)";
+        item.querySelector('h6').style.display = "block";
+        item.querySelector('p').style.display = "none";
+        item.querySelector('img').style.display = "none";
+    })
+})
+
+
+
+
+
+//Finish #index-third-section
